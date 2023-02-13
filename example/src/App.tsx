98edea4589eb56183f077ee9cef068f08a4f53b5
stephenhackettdelaney <stephenhackettdelaney@gmail.com>
2023-02-13 17:46:29 -0600

    test radio group

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,10 +2,37 @@ import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
 
+import { RadioGroup, RadioButton } from 'cmdn-choice-group';
+
 export default function App() {
+  const data = [
+    { label: 'HTML', value: 'html' },
+    { label: 'CSS', value: 'css' },
+    { label: 'Javascript', value: 'javascript' },
+  ];
+
+  function onPress() {
+    console.log('log');
+  }
+
   return (
     <View style={styles.container}>
       <Text>Result</Text>
+      <RadioGroup initialValues={data}>
+        {data.map(({ label, value }) => {
+          return (
+            <RadioButton key={label} value={value} onPress={onPress}>
+              {() => {
+                return (
+                  <View>
+                    <Text>{value}</Text>
+                  </View>
+                );
+              }}
+            </RadioButton>
+          );
+        })}
+      </RadioGroup>
     </View>
   );
 }
